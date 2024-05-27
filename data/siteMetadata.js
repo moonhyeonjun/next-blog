6bdffbec86bec7569b9102aa8aaa5d9c746316fb
moonhyeonjun <mhj5256@gmail.com>
2024-05-27 20:11:06 +0900

    [Chore] 사이트 메타데이터 수정

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Hayden Dev Blog',
+  author: 'Hyeonjun Moon',
+  headerTitle: 'Hayden Moon',
+  description: 'A blog about software engineering, web development, and other tech-related topics.',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
