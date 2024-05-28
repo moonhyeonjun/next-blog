c2fc3f2c404d763b3add5a2874a519582210ce3a
moonhyeonjun <mhj5256@gmail.com>
2024-05-28 13:15:12 +0900

    [refactor] project 설명 및 이미지 추가

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,26 +1,22 @@
 interface Project {
-  title: string,
-  description: string,
-  href?: string,
-  imgSrc?: string,
+  title: string
+  description: string
+  href?: string
+  imgSrc?: string
 }
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'AntV X6 diagram',
+    description: `AntV X6 diagram is a product that has features such as creating and connecting diagrams, selecting nodes, and auto-arranging.`,
+    imgSrc: '/static/images/projects/antv-x6-diagram.png',
+    href: 'https://craft-n8cr.vercel.app/craft/antv-x6-diagram',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'React Flow diagram',
+    description: `React Flow diagram is a tool that offers features such as type changes through dragging, node connection, grouping, and more.`,
+    imgSrc: '/static/images/projects/reactflow-diagram.png',
+    href: 'https://craft-n8cr.vercel.app/craft/reactflow-diagram',
   },
 ]
 
