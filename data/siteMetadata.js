cef9e898c211a4da12d30fe830edb23ada00acc3
moonhyeonjun <mhj5256@gmail.com>
2024-06-12 13:31:01 +0900

    [chore] 사이트 주소 및 레포 변경

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,8 +6,8 @@ const siteMetadata = {
   description: 'A blog about software engineering, web development, and other tech-related topics.',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://www.moonhyeonjun.com',
+  siteRepo: 'https://github.com/moonhyeonjun/next-blog',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
